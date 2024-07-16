5b6825a286c519bb6a4dba1c13c4f71fa970b18d
Hans Trauwitz <hans.trauwitz@gmail.com>
2024-07-16 19:01:38 -0400

    adding logo to images folder and changing the phone number in the footer

--- components/Footer.js
+++ components/Footer.js
@@ -5,7 +5,7 @@ export default function Footer() {
         <div className="container mx-auto text-center font-aeonik-light">
           <p>&copy; 2024 ProTarget. All rights reserved.</p>
           <p className="mt-2">
-            <a href="tel:+12122578400" className="hover:underline">+1 212-257-8400</a> | 
+            <a href="tel:+15146191358" className="hover:underline">+1 (514) 619 1358</a> | 
             <a href="mailto:info@protarget.com" className="hover:underline ml-2">info@protarget.com</a>
           </p>
         </div>
